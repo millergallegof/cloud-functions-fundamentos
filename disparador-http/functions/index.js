bb1e618ad7ef49e87c98545a7ad92604d2179d0d
millergallegof <miller.gallegof@gmail.com>
2023-01-03 12:51:10 -0500

    ejemplo con express

--- disparador-http/functions/index.js
+++ disparador-http/functions/index.js
@@ -1,9 +1,42 @@
 const functions = require("firebase-functions");
+const express = require('express');
+const cors = require('cors');
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+app.use(cors({ origin: true }))
+
+app.get('/:id', (req, resp) => resp.send(getUserById(req.params.id)))
+app.post('/', (req, resp) => {
+    createUser(req.body.name, req.body.email)
+    resp.status(201).send({ successful: true })
+
+})
+
+
+// disparador request
+// request es una peticion http no importa cual
+
+exports.createUser = functions.https.onRequest((req, resp) => {
+    functions.logger.info(req.body.name)
+    functions.logger.info(req.body.email)
+    functions.logger.info(req.body.phone)
+
+    resp.status(200).json({ successful: true })
+})
+
+
+const getUserById = (userId) => {
+    return {
+        id: userId,
+        name: "Miller Gallego",
+        email: 'miller.gallegof@gmail.com'
+    }
+}
+
+const createUser = (name, email) => {
+    return {
+        id: '121',
+        name: name,
+        email: email
+    }
+}
